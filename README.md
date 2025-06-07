d18f58944f1795d99c983d9169655e13c873e064
abba <adjiabba10@gmail.com>
2025-06-07 18:05:56 +0100

    Storage settings commit
    Awss3 config
    
     On branch main

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+ARN
 
+arn:aws:iam::233720365922:user/resto-eyang
+Console access
+Disabled
+Access key 1
+Create access key
+Created
+June 07, 2025, 14:02 (UTC+01:00)
+Last console sign-in
--- Restaurant_reserve/settings.py
+++ Restaurant_reserve/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Myrestaurant',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY') 
+AWS_STORAGE_BUCKET_NAME = 'restaurant-reserve-media'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_SIGNATURE_NAME= 's3v4'
+AWS_S3_FILE_OVERWRITE= False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY= True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = '/media/'
